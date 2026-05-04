7abd17287b4bddde53fe2fc9c4a78a57fbd34d2a
Sumit Kumar <inventor@outlook.in>
2026-05-04 09:02:10 +0530

    Add Next.js middleware for auth

---
+++ middleware.ts
@@ -0,0 +1,6 @@
+import { updateSession } from './src/lib/supabase/middleware'
+import { type NextRequest } from 'next/server'
+export async function middleware(request: NextRequest) {
+  return await updateSession(request)
+}
+export const config = { matcher: ['/((?![^/]*\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'] }
